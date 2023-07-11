395add21fc39599eb70fb77e44fa0dc5104bed1b
20120050 <dang_252@yahoo.com.vn>
2023-07-11 13:56:09 +0700

    deplpoy 1

--- myforms/src/app/sign-up/page.tsx
+++ myforms/src/app/sign-up/page.tsx
@@ -1,16 +1,10 @@
 import SignUpForm from "../components/SignUpForm";
 
-
-interface NameProps {
-    propName: string;
-}
-
-
 export const metadata = {
     title: 'Sign up',
 };
 
-export default function SignUp({ }: NameProps) {
+export default function SignUp() {
     return (
         <div className="flex justify-center items-center min-h-screen p-8 mt-[71px] bg-[url('/bg-light-2.jpg')] dark:bg-[url('/bg-dark-2.jpg')] bg-cover shadow-lg relative">
             <SignUpForm />
